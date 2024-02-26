7cfccdf9dae75a0fb026a1ae0ba3a6f5444fe8e6
prikol769 <prikol769@gmail.com>
2024-02-26 16:15:18 -0600

    added Header component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationsTypePage = () => {
-  return <div>AddTransformationsTypePage</div>;
+const AddTransformationsTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  return (
+    <Header title={transformation.title} subTitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationsTypePage;
