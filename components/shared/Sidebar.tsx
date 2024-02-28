967058dbd89642efcf3f326c1c7d8c9b71dca812
prikol769 <prikol769@gmail.com>
2024-02-28 16:23:04 -0600

    fix Sidebar login button

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -84,7 +84,7 @@ const Sidebar = () => {
 
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in"></Link>
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </nav>
